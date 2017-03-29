223d60a88ab75a83aef032f936574863cf5704dd
Craig Sturzaker <craig.sturzaker@arup.com>
2017-03-29 18:42:54 +0100

    Added 404 page

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -49142,6 +49142,26 @@ module.exports = {
 'use strict';
 
 var React = require('react');
+var Link = require('react-router').Link;
+
+var FourOFour = React.createClass({displayName: "FourOFour",
+    render: function() {
+        return (
+            React.createElement("div", null, 
+                React.createElement("h1", null, "Page Not Found"), 
+                React.createElement("p", null, "Whoops! Sorry, there is nothing to see here"), 
+                React.createElement("p", null, React.createElement(Link, {to: "app"}, "Back to Home"))
+            )
+        )
+    }
+});
+
+module.exports = FourOFour;
+
+},{"react":197,"react-router":28}],201:[function(require,module,exports){
+'use strict';
+
+var React = require('react');
 
 var About = React.createClass({displayName: "About",
     render: function() {
@@ -49166,7 +49186,7 @@ var About = React.createClass({displayName: "About",
 
 module.exports = About;
 
-},{"react":197}],201:[function(require,module,exports){
+},{"react":197}],202:[function(require,module,exports){
 /*eslint-disable strict */ //Disabling check because we can't run strict mode.  Need global jQuery for bootstrap
 
 var React = require('react');
@@ -49189,7 +49209,7 @@ var App = React.createClass({displayName: "App",
 
 module.exports = App;
 
-},{"./common/header":204,"jquery":2,"react":197,"react-router":28}],202:[function(require,module,exports){
+},{"./common/header":205,"jquery":2,"react":197,"react-router":28}],203:[function(require,module,exports){
 'use strict';
 
 var React = require('react');
@@ -49224,7 +49244,7 @@ var AuthorList = React.createClass({displayName: "AuthorList",
 });
 
 module.exports = AuthorList;
-},{"react":197}],203:[function(require,module,exports){
+},{"react":197}],204:[function(require,module,exports){
 'use strict';
 
 var React = require('react');
@@ -49257,7 +49277,7 @@ var AuthorPage = React.createClass({displayName: "AuthorPage",
 });
 
 module.exports = AuthorPage;
-},{"../../api/authorApi":198,"./authorList":202,"react":197}],204:[function(require,module,exports){
+},{"../../api/authorApi":198,"./authorList":203,"react":197}],205:[function(require,module,exports){
 'use strict';
 
 var React = require('react');
@@ -49282,7 +49302,7 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header;
 
-},{"react":197,"react-router":28}],205:[function(require,module,exports){
+},{"react":197,"react-router":28}],206:[function(require,module,exports){
 'use strict';
 
 //import react
@@ -49301,7 +49321,7 @@ var Home = React.createClass({displayName: "Home",
 });
 
 module.exports = Home;
-},{"react":197}],206:[function(require,module,exports){
+},{"react":197}],207:[function(require,module,exports){
 'use strict';
 var React = require('react');
 var Router = require('react-router');
@@ -49310,21 +49330,23 @@ var routes = require('./routes');
 Router.run(routes, function(Handler) {
     React.render(React.createElement(Handler, null), document.getElementById('app'));
 });
-},{"./routes":207,"react":197,"react-router":28}],207:[function(require,module,exports){
+},{"./routes":208,"react":197,"react-router":28}],208:[function(require,module,exports){
 'use strict';
 var React = require('react');
 var Router = require('react-router');
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
 
 var routes = (
     React.createElement(Route, {name: "app", path: "/", handler: require('./components/app')}, 
         React.createElement(DefaultRoute, {handler: require('./components/homePage')}), 
         React.createElement(Route, {name: "authors", handler: require('./components/authors/authorPage')}), 
-        React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')})
+        React.createElement(Route, {name: "about", handler: require('./components/about/aboutPage')}), 
+        React.createElement(NotFoundRoute, {handler: require('./components/FourOFour')})
     )
 );
 
 module.exports = routes;
 
-},{"./components/about/aboutPage":200,"./components/app":201,"./components/authors/authorPage":203,"./components/homePage":205,"react":197,"react-router":28}]},{},[206]);
+},{"./components/FourOFour":200,"./components/about/aboutPage":201,"./components/app":202,"./components/authors/authorPage":204,"./components/homePage":206,"react":197,"react-router":28}]},{},[207]);
