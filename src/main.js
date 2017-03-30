b297b6c21e359556cf1256faa2eaa079f3318860
Craig Sturzaker <craig.sturzaker@arup.com>
2017-03-30 12:25:18 +0100

    HistoryLocation to remove hashes from route URLs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
 
-Router.run(routes, function(Handler) {
+//HistoryLocation removes hashes
+Router.run(routes, Router.HistoryLocation, function(Handler) {
     React.render(<Handler/>, document.getElementById('app'));
 });
